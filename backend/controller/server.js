05b5975e24cb9f39f6f88012c4162940f50200cb
SHAI GUNDERSEN <shai.gundersen@gmail.com>
2021-05-27 01:16:51 +0300

    added form + server validation

--- backend/controller/server.js
+++ backend/controller/server.js
@@ -16,11 +16,39 @@ app.get("/", (req, res) => {
   //res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
-app.post("/detect", (req, res) => {
-  // need to add validation check
+function validateBody(data) {
+  let isValid = true;
+  const error = new Error();
+  error.status = 400;
+  if (!Object.keys(data).length) {
+    error.massage = "Missing Body In Request !";
+  } else if (!data.detectorType) {
+    error.massage = "Missing detectorType !";
+  } else if (!data.learn) {
+    error.massage = "Missing Learn Json !";
+  } else if (!data.anomaly) {
+    error.massage = "Missing Anomaly Json !";
+  } else {
+    try {
+      let learnJson = JSON.parse(data.learn);
+      let testJson = JSON.parse(data.anomaly);
+      return { learnJson, testJson };
+    } catch {
+      error.massage = "Not Supported Json Format !";
+    }
+  }
+  if (error.massage) return error;
+}
+
+app.post("/detect", (req, res, next) => {
   let data = req.body;
-  let learnJson = JSON.parse(data.learn);
-  let testJson = JSON.parse(data.anomaly);
+  const isValid = validateBody(data);
+  if (isValid.massage) {
+    next(isValid);
+    return;
+  }
+  let learnJson = isValid.learnJson;
+  let testJson = isValid.testJson;
   let algoType = data.detectorType;
   // returns a Promise with Json report
   model
@@ -29,7 +57,26 @@ app.post("/detect", (req, res) => {
       res.status(200).send(anomalyReport);
       res.end();
     }) // need to handle error better.. maybe add status 500 and a page?
-    .catch((error) => console.log(error));
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+// handle not supported paths
+app.use((req, res, next) => {
+  const error = new Error();
+  error.status = 404;
+  error.massage = "Not Found";
+  next(error);
+});
+// handle all kinds of different errors thrown by other paths
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      massage: error.massage,
+    },
+  });
 });
 
 app.listen(8080, () => {
--- frontend/src/components/inputForm.jsx
+++ frontend/src/components/inputForm.jsx
@@ -7,61 +7,98 @@ import Card from "react-bootstrap/Card";
 
 import "../style.css";
 import axios from "axios";
-
 class InputForm extends Component {
-  state = {
-    detectorType: "",
-    learnFileName: "Please Upload Learn CSV",
-    anomalyFileName: "Please Upload Anomaly CSV",
-    learnFile: null, // learn file
-    anomalyFile: null, // test file
-    anomalyReport: null,
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      detectorType: "",
+      learnFileName: "Please Upload Learn CSV",
+      anomalyFileName: "Please Upload Anomaly CSV",
+      learnFile: null, // learn file
+      anomalyFile: null, // test file
+      anomalyReport: null,
+      learnFileError: false, // for validation
+      anomalyFileError: false, // for validation
+    };
+    // preserve the initial state to fall back to
+    this.baseState = this.state;
+  }
   selectionChanged = (event) => {
     this.setState({ detectorType: event.target.value });
   };
 
-  // need to add some comments here..
+  csvToJson(file, fileDataProperty) {
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.readAsText(file, "UTF-8");
+    fileReader.onload = () => {
+      var lines = fileReader.result.split(/\r\n|\r|\n/g); //split by new line depending on system
+      var result = [];
+      var headers = lines[0].split(",");
+      var len = lines.length;
+      for (var i = 0; i < headers.length; i++) {
+        var obj = {};
+        var values = [];
+        for (var j = 1; j < len - 1; j++) {
+          var currentline = lines[j].split(",");
+          values.push(parseFloat(currentline[i]));
+        }
+        obj[headers[i]] = values;
+        result.push(obj);
+      }
+      var json = JSON.stringify(result); //JSON
+      json = json.replaceAll("{", "");
+      json = json.replaceAll("}", "");
+      json = json.substring(1, json.length - 1); // remove wrapping []
+      json = "{" + json + "}";
+      this.setState({ [fileDataProperty]: json });
+    };
+  }
+  // file selected handler
   fileSelected = (event) => {
     let file = event.target.files[0];
     let file_type = file.type; //getting selected file type
     let valid_extensions = ["application/vnd.ms-excel", "csv"]; //adding some valid csv extensions in array
+    let fileNameProperty = event.target.name;
+    let fileName = event.target.files[0].name;
+    console.log(fileNameProperty);
+    console.log(this.state.learnFileName);
+    this.setState({ [fileNameProperty]: fileName });
+    console.log(this.state.learnFileName);
+    let fileDataProperty = fileNameProperty.replace("Name", "");
+    let fileError = fileNameProperty.replace("Name", "Error");
     if (valid_extensions.includes(file_type)) {
-      let fileNameProperty = event.target.name;
-      let fileName = event.target.files[0].name;
-      this.setState({ [fileNameProperty]: fileName });
-      let fileDataProperty = fileNameProperty.replace("Name", "");
-      let fileReader = new FileReader(); //creating new FileReader object
-      fileReader.readAsText(file, "UTF-8");
-      fileReader.onload = () => {
-        var lines = fileReader.result.split("\r\n");
-        var result = [];
-        var headers = lines[0].split(",");
-        var len = lines.length;
-        for (var i = 0; i < headers.length; i++) {
-          var obj = {};
-          var values = [];
-          for (var j = 1; j < len - 1; j++) {
-            var currentline = lines[j].split(",");
-            values.push(parseFloat(currentline[i]));
-          }
-          obj[headers[i]] = values;
-          result.push(obj);
-        }
-        var json = JSON.stringify(result); //JSON
-        json = json.replaceAll("{", "");
-        json = json.replaceAll("}", "");
-        json = json.substring(1, json.length - 1); // remove wrapping []
-        json = "{" + json + "}";
-        this.setState({ [fileDataProperty]: json });
-      };
+      // set error to be false
+      this.setState({ [fileError]: false });
+      // valid extention -> make json of it
+      this.csvToJson(file, fileDataProperty);
+    } else {
+      // invalid type -> raise error
+      this.setState({ [fileError]: true });
     }
+    event.target.value = null;
   };
+
+  setDefaultState() {
+    this.setState(this.baseState);
+  }
+  // if any of conditions are satisfied -> form is incomplete -> disable submit button
+  checkFormValidation() {
+    const {
+      detectorType,
+      learnFileName,
+      anomalyFileName,
+      learnFileError,
+      anomalyFileError,
+    } = this.state;
+    if (detectorType === this.baseState.detectorType) return true;
+    if (learnFileName === this.baseState.learnFileName || learnFileError)
+      return true;
+    if (anomalyFileName === this.baseState.anomalyFileName || anomalyFileError)
+      return true;
+  }
 
   submitHandler = (event) => {
     event.preventDefault();
-    alert("submitted files!!!!");
     axios
       .post("/detect", {
         detectorType: this.state.detectorType,
@@ -72,63 +109,80 @@ class InputForm extends Component {
         this.setState({ anomalyReport: response.data.report });
       })
       .catch((err) => console.log(err));
+    //set the form back to default
+    this.setDefaultState();
   };
 
   render() {
     return (
-      <Form className="m-3 items mainForm" onSubmit={this.submitHandler}>
-        <Row>
-          <Col xs="auto">
-            <Form.Control
-              as="select"
-              className="mr-sm-2"
-              onChange={this.selectionChanged}
-              custom
-            >
-              <option value="">Choose Algorithem...</option>
-              <option value="regression">Regression</option>
-              <option value="hybrid">Hybrid</option>
-            </Form.Control>
-          </Col>
-          <Col>
-            <Form.Group>
-              <Form.File
-                className="inputFile"
-                id="custom-file"
-                label={this.state.learnFileName}
-                onInput={this.fileSelected}
-                name="learnFileName"
+      <React.Fragment>
+        <Form
+          className="m-3 items mainForm needs-validation"
+          noValidate
+          onSubmit={this.submitHandler}
+        >
+          <Row>
+            <Col xs="auto">
+              <Form.Control
+                as="select"
+                className="mr-sm-2"
+                onChange={this.selectionChanged}
                 custom
-              ></Form.File>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group>
-              <Form.File
-                className="inputFile"
-                id="custom-file"
-                label={this.state.anomalyFileName}
-                name="anomalyFileName"
-                onInput={this.fileSelected}
-                custom
-              ></Form.File>
-            </Form.Group>
-          </Col>
-          <Col>
-            <Button type="submit">Submit</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Card>
-            <Card.Body>
-              {/* prints report iff not null */}
-              {this.state.anomalyReport
-                ? JSON.stringify(this.state.anomalyReport)
-                : ""}
-            </Card.Body>
-          </Card>
-        </Row>
-      </Form>
+                value={this.state.detectorType}
+              >
+                <option value="">Choose Algorithem...</option>
+                <option value="regression">Regression</option>
+                <option value="hybrid">Hybrid</option>
+              </Form.Control>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.File className="inputFile" id="custom-file" custom>
+                  <Form.File.Input
+                    name="learnFileName"
+                    onInput={this.fileSelected}
+                    isInvalid={this.state.learnFileError}
+                  />
+                  <Form.File.Label>{this.state.learnFileName}</Form.File.Label>
+                  <Form.Control.Feedback type="invalid">
+                    file is not CSV type!
+                  </Form.Control.Feedback>
+                </Form.File>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.File className="inputFile" id="custom-file" custom>
+                  <Form.File.Input
+                    name="anomalyFileName"
+                    onInput={this.fileSelected}
+                    isInvalid={this.state.anomalyFileError}
+                  />
+                  <Form.File.Label>
+                    {this.state.anomalyFileName}
+                  </Form.File.Label>
+                  <Form.Control.Feedback type="invalid">
+                    file is not CSV type!
+                  </Form.Control.Feedback>
+                </Form.File>
+              </Form.Group>
+            </Col>
+            <Col>
+              <Button type="submit" disabled={this.checkFormValidation()}>
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        <Card>
+          <Card.Body>
+            {/* prints report iff not null */}
+            {this.state.anomalyReport
+              ? JSON.stringify(this.state.anomalyReport)
+              : ""}
+          </Card.Body>
+        </Card>
+      </React.Fragment>
     );
   }
 }
